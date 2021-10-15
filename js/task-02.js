8236c9c286bc69d4de384db553fb08cdc1bb8db2
H.Long Nguyen <84514305+HLongNguyen0@users.noreply.github.com>
2021-10-15 20:44:44 +0300

    v1.2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,8 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+for (let ingredient of ingredients) {
+  const element = document.createElement('li');
+  element.textContent = ingredient;
+  element.classList.add('item');
+  document.querySelector('#ingredients').append(element);
+}
