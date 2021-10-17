60d6d3e946c7cd677746b9d519775ada52cd1204
H.Long Nguyen <84514305+HLongNguyen0@users.noreply.github.com>
2021-10-17 21:14:58 +0300

    v1.8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector('.login-form');
 
+class acc {
+    constructor({ email, password }) {
+        this.email = email;
+        this.password = password;
+    }
+}
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const { email, password } = event.currentTarget;
+    const user = new acc({email: email.value, password: password.value});
+    if (email.value && password.value) {
+        console.log(user);
+    } else {
+        alert('wtf man ?')
+    }
+})
