88a7b40e29a45abbb74ad36a87eddd4ca641ab7c
H.Long Nguyen <84514305+HLongNguyen0@users.noreply.github.com>
2021-10-17 21:38:26 +0300

    1.9.1
    fix task 6

--- js/task-06.js
+++ js/task-06.js
@@ -2,11 +2,12 @@ const input = document.querySelector('#validation-input');
 
 input.addEventListener('input', () => {
     input.style.outline = 'none';
-    if (input.value.length < 6) {
+    if (input.value.length < input.dataset.length) {
         input.classList.remove('valid');
         input.classList.remove('invalid');
     } else if (input.value.length == input.dataset.length) {
         input.classList.add('valid');
+        input.classList.remove('invalid');
     } else {
         input.classList.add('invalid')
     }
